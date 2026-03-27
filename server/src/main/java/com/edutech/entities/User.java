5d4677a6627559e9d4a948a1434f3d67a97d975c
NivethaSS253 <dummy@gmail.com>
2026-03-27 12:50:11 +0000

    evet,eventplanner,user

--- server/src/main/java/com/edutech/entities/User.java
+++ server/src/main/java/com/edutech/entities/User.java
@@ -1,11 +1,68 @@
 package com.edutech.entities;
-
 import javax.persistence.*;
 import java.io.Serializable;
 
 
-public class User implements Serializable {
+@Entity
+@Table(name = "users")
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(
+        name = "user_type",
+        discriminatorType = DiscriminatorType.STRING
+)
+public class User implements Serializable{
 
-   // write the code here
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String password;
+
+    // "EventPlanner", "Staff", "Client"
+    private String role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+ 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+ 
+    public String getRole() {
+        return role;
+    }
+ 
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
-
